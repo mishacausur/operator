059ea8e3182765c8500b453533b6ebe5a67f9ba0
Misha Causur <mishacausur@mail.ru>
2022-02-20 16:13:01 +0500

    screen size factored

--- kernel.c
+++ kernel.c
@@ -7,8 +7,16 @@
 
 #include "kernel.h"
 
+#define LINES 25
+#define COLUMNS 80
+#define SCREEN_SIZE LINES * COLUMNS
+
 int current_loc = 0;
 char *vidptr = (char*)0xb8000;
+
+void clear_screen(void) {
+    int i = 0
+}
 
 void kprint(const char *str) {
     int i = 0;
